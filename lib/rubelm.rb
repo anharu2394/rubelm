02a43c30adfebb2eb49d35ae7ea69f5ae0545d78
安藤春香 <haruan2394@gmail.com>
2018-11-10 14:43:32 +0900

    Add: lib

---
+++ lib/rubelm.rb
@@ -0,0 +1,9 @@
+require "rubelm/version"
+require "rubelm/opal"
+#module Rubelm
+  #module View
+  #  require "rubelm/view"
+  #end
+  #def main(view)
+  #end
+#end
---
+++ lib/rubelm/version.rb
@@ -0,0 +1,3 @@
+module Rubelm
+  VERSION = "0.1.0"
+end
