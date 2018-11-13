52a1793e8967268374d56353bb1f89b0a9bb0d26
Anharu <haruan2394@gmail.com>
2018-11-13 18:51:28 +0900

    Change: test name

--- spec/opal/vdom_spec.rb
+++ spec/opal/vdom_spec.rb
@@ -10,34 +10,51 @@ describe "test vdom" do
       $document.body.element_children.to_ary[1].clear
     end
     let(:root) {$document.body.element_children.to_ary[1]}
-    it 'div' do
-      a = Rubelm::Vdom::render(div(),root)
-      expect(a.child.name).to eq("DIV")
     let(:root_child) {$document.body.element_children.to_ary[1].elements.to_ary[0]}
+    describe 'div' do
+      it 'div' do
+        a = Rubelm::Vdom::render(div(),root)
+        expect(a.child.name).to eq("DIV")
+      end
+      it 'attributes only class' do
+        a = Rubelm::Vdom::render(div({class: "test"}),root)
+        expect(a.child[:class]).to eq("test")
+      end
+      it 'attributes' do
+        a = Rubelm::Vdom::render(div({class: "test",id:"t"}),root)
+        expect(a.child[:class]).to eq("test")
+        expect(a.child[:id]).to eq("t")
+      end
+      it 'many attributes' do
+        a = Rubelm::Vdom::render(div({class: "testatt",id:"t", "data-url": "/opal"}), root)
+        expect(a.child[:class]).to eq("testatt")
+        expect(a.child[:id]).to eq("t")
+        expect(a.child["data-url"]).to eq("/opal")
+      end
+      it 'include text' do
+        a = Rubelm::Vdom::render(div({},"hello"),root)
+        expect(a.child.text). to eq("hello")
+      end
+      it 'include child' do
+        a = Rubelm::Vdom::render(div({id: "outer"},[div({id: "inner"})]),root)
+        expect(a.child[:id]).to eq("outer")
+        expect(a.child.child[:id]).to eq("inner")
+      end
+      it 'include child and text' do
+        a = Rubelm::Vdom::render(div({id: "outer"},[div({id: "inner"},"inner")]),root)
+        expect(a.child[:id]).to eq("outer")
+        expect(a.child.child[:id]).to eq("inner")
+        expect(a.child.child.text).to eq("inner")
+      end
     end
-    it 'div attributes only class' do
-      a = Rubelm::Vdom::render(div({class: "test"}),root)
-      expect(a.child[:class]).to eq("test")
+    describe 'p' do
+      it 'include child and text' do
+        a = Rubelm::Vdom::render(div({id: "outer"},[p({id: "inner"},"inner")]),root)
+        expect(a.child[:id]).to eq("outer")
+        expect(a.child.child[:id]).to eq("inner")
+        expect(a.child.child.text).to eq("inner")
+      end
     end
-    it 'div attributes' do
-      a = Rubelm::Vdom::render(div({class: "test",id:"t"}),root)
-      expect(a.child[:class]).to eq("test")
-      expect(a.child[:id]).to eq("t")
-    end
-    it 'div many attributes' do
-      a = Rubelm::Vdom::render(div({class: "testatt",id:"t", "data-url": "/opal"}), root)
-      expect(a.child[:class]).to eq("testatt")
-      expect(a.child[:id]).to eq("t")
-      expect(a.child["data-url"]).to eq("/opal")
-    end
-    it 'div include text' do
-      a = Rubelm::Vdom::render(div({},"hello"),root)
-      expect(a.child.text). to eq("hello")
-    end
-    it 'div include child' do
-      a = Rubelm::Vdom::render(div({id: "outer"},[div({id: "inner"})]),root)
-      expect(a.child[:id]).to eq("outer")
-      expect(a.child.child[:id]).to eq("inner")
     end
   end
 end
