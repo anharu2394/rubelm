8970c79adb0a3263616a097ce5685f8abb321a3a
Anharu <haruan2394@gmail.com>
2018-11-13 21:11:01 +0900

    Change: children should be Array if including text

--- opal/rubelm/html.rb
+++ opal/rubelm/html.rb
@@ -3,11 +3,19 @@ module Rubelm::Html
   def self.def_tags(*tags)
     tags.each do |tag_name|
       define_method(tag_name) {|attributes = {}, children = []| 
-        node = {
-          nodeName: tag_name,
-          attributes: attributes,
-          children: children
-        }
+        if children.instance_of?(Array)
+          node = {
+            nodeName: tag_name,
+            attributes: attributes,
+            children: children
+          }
+        else
+          node = {
+            nodeName: tag_name,
+            attributes: attributes,
+            children: [children]
+          }
+        end
         node
       }
     end
