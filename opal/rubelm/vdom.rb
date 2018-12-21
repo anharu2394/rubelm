cb9009e49f759ae9a39095e6187b827458e7a6d1
Anharu <haruan2394@gmail.com>
2018-12-21 13:49:02 +0900

    Change: code miss on patch func

--- opal/rubelm/vdom.rb
+++ opal/rubelm/vdom.rb
@@ -33,12 +33,13 @@ module Rubelm::Vdom
     if old_node[:nodeName] != new_node[:nodeName]
       ele.remove
       create(new_node,root)
-    elsif ele.attributes.namespace != new_node[:attributes]
-      ele.attributes.merge!(new_node[:attributes])
-      root
-    end
-    new_node[:children].each_with_index do |child, i|
-      child.instance_of?(Hash) ? patch(old_node[:children[i], child, ele]) : ele.text = child
+    else
+      if ele.attributes.namespace != new_node[:attributes]
+        ele.attributes.merge!(new_node[:attributes])
+      end
+      new_node[:children].each_with_index do |child, i|
+        child.instance_of?(Hash) ? patch(old_node[:children][i], child, ele) : ele.text = child
+      end
     end
     root
   end
