9c5ade672d56244b777e522813918928da9f33a2
Anharu <haruan2394@gmail.com>
2019-01-03 11:03:22 +0900

    Add: check argrement

--- opal/rubelm/vdom/vnode.rb
+++ opal/rubelm/vdom/vnode.rb
@@ -1,14 +1,12 @@
 class Rubelm::VDOM::VNode
-  attr_reader :node_name, :attributes, :children
+  attr_reader :name, :attributes, :children
   def initialize(name, attributes, children)
-    if !attributes.instance_of?(Hash)
-      raise ArgumentError, "attributes must be Hash instance."
-    end
-    if !children.instance_of?(Array)
-      raise ArgumentError, "children must be Array instance."
-    end
-    @node_name = name
-    @attributes = attributes
-    @children = children
+    # Argument check
+    raise ArgumentError, "attributes must be Hash instance." unless attributes.respond_to? :to_h
+    raise ArgumentError, "children must be Array instance."  unless children.respond_to? :to_a
+    
+    @attributes = attributes.to_h
+    @children = children.to_a
+    @name = name
   end
 end
