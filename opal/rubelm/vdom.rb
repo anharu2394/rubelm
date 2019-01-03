8588821fc3a3416404127081ad67b3066a47ba7a
Anharu <haruan2394@gmail.com>
2019-01-03 11:04:33 +0900

    Change: Hash to VNode

--- opal/rubelm/vdom.rb
+++ opal/rubelm/vdom.rb
@@ -23,23 +23,19 @@ module Rubelm::VDOM
     ele.children.to_ary.each do |node|
       node.instance_of?(Browser::DOM::Element) ? children << recycle(node) : children << node.whole
     end
-    node = {
-      nodeName: ele.name.downcase,
-      attributes: attributes,
-      children: children
-    }
+    VNode.new(ele.name.downcase, attributes, children)
   end
   def self.patch(old_node,new_node,root)
     ele = root.elements.to_ary[0]
-    if old_node[:nodeName] != new_node[:nodeName]
+    if old_node.name != new_node.name
       ele.remove
       create(new_node,root)
     else
-      if ele.attributes.namespace != new_node[:attributes]
-        ele.attributes.merge!(new_node[:attributes])
+      if ele.attributes.namespace != new_node.attributes
+        ele.attributes.merge!(new_node.attributes)
       end
-      new_node[:children].each_with_index do |child, i|
-        child.instance_of?(Hash) ? patch(old_node[:children][i], child, ele) : ele.text = child
+      new_node.children.each_with_index do |child, i|
+        child.instance_of?(VNode) ? patch(old_node.children[i], child, ele) : ele.text = child
       end
     end
     root
