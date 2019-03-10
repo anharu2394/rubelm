f22271122bb7712d20dbe1410060806a6e8cd2f7
Anharu <haruan2394@gmail.com>
2019-03-10 20:55:21 +0900

    Add: test state in class

--- test/state_test.rb
+++ test/state_test.rb
@@ -11,14 +11,24 @@ class VDOMRenderTest < Opal::Test::Unit::TestCase
     @@test_body << Browser::DOM::Element.create('div').add_class('vdom-test')
     @@root = @@test_body.children.to_ary[0]
   end
-  test 'state' do
+  test 'state can see in html' do
     state = Struct.new(:count)
     initial_state = state.new(0)
     view = component do |state, _actions|
       div({}, state.count)
     end
     a = Rubelm::VDOM.render(view, initial_state, nil, @@root)
-    assert_equal(a.child.value, '0')
+    assert_equal('0', a.child.inner_html)
+    @@root.remove
+  end
+  test 'state can see in class' do
+    state = Struct.new(:name)
+    initial_state = state.new('anharu')
+    view = component do |state, _actions|
+      div(class: state.name)
+    end
+    a = Rubelm::VDOM.render(view, initial_state, nil, @@root)
+    assert_equal('0', a.child.inner_html)
     @@root.remove
   end
 end
